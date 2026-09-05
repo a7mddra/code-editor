eea92153c4c526bfa42442e828497a5fbaa51051
a7mddra <a7mddra@gmail.com>
2026-09-06 00:11:04 +0300

    refactor: remove unused dialogs.css import from main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./styles/main.css";
-
 import "./styles/ide.css";
-import "./styles/dialogs.css";
 
 // Monaco environment configuration
 window.MonacoEnvironment = {
